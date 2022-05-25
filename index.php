c360871ca24c2aecc4a940ea3ae84e8ae7e53d62
Cleyber <cleyber.fernandes@gmail.com>
2022-05-25 06:39:41 -0300

    creating functions for routes

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ require __DIR__ . "/src/Router.php";
 
 $route = new Router(__DIR__ . "router", ":");
 
+//$route->namespace("test");
 $route->get("/rotas/{data}/{teste}/{page}", "TestController:list");
